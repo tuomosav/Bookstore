add47a9b4ac358321378c7628700c6a89407d250
tuomo.savolainen@myy.haaga-helia.fi <tuomo.savolainen@myy.haaga-helia.fi>
2025-01-30 02:52:54 +0200

    29.1. Tunti harjoitukset 1-5 valmiit

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import k25.bookstore.domain.BookRepository;
 import k25.bookstore.domain.Book;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,28 +23,40 @@ public class BookController {
         this.repository = repository;
     }
 
+    //Show all books
     @GetMapping("/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
     
+    //Add new book
     @RequestMapping(value = "/addbook")
     public String addBook(Model model) {
         model.addAttribute("book", new Book());
         return "addbook";
     }
     
+    //Save new book
     @PostMapping("/save")
     public String save(Book book) {
         repository.save(book);
         return "redirect:booklist";
     }
     
+    //Delete book
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         repository.deleteById(bookId);
         return "redirect:../booklist";
     }
+
+    //Edit book
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    }
+    
 }
     
